e8b18548ad923f7e359c38071c748f2bf56fd216
Rodrigo Sampaio <rodrigo.sampaio@tradeforce.com>
2018-11-20 17:19:36 -0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda - builded by CodeDeploy!'),
+        body: JSON.stringify('Hello from Lambda - builded by CodeBuild!'),
     };
     return response;
 };
